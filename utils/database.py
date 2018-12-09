ef706cbd201f09003d66c1356b5a3b915ea0e2a5
root <root@cs395-vm-vm.us-west1-b.c.cs395t-final-project.internal>
2018-12-09 07:37:21 +0000

    hand data structure

--- utils/database.py
+++ utils/database.py
@@ -2,7 +2,7 @@ import psycopg2
 import pickle
 import ast
 import os
-from utils.config import config
+from config import config
  
 class ModelReader:
     """ModelReader class
@@ -94,7 +94,7 @@ class ModelReader:
         if self.grasp_cur == None:
             self.grasp_cur = self.query()
         try:
-            grasp_batch = list(next(self.grasp_cur) for _ in range(self.batch_size))
+            grasp_batch = list(next(self.grasp_cur) for _ in range(batch_size))
         except StopIteration:
             print ("Ran through all batches. Re-querying")
             self.grasp_cur = self.query()
@@ -133,15 +133,18 @@ class ModelReader:
 
         # Query original model
         sql_orig_model = """
-            Select original_model_geometry_path 
+            Select original_model_geometry_path,
+            original_model_grasping_rescale
             from public.original_model
             WHERE original_model_id=""" + str(scale_dict['original_model_id'])
         cur.execute(sql_orig_model)
-        model_file_path = cur.fetchone()[0]
+        res = cur.fetchone()
+        model_file_path = res[0]
+        model_rescale = res[1]
        
         # close the communication with the PostgreSQL
         cur.close()
-        return scale_dict['scaled_model_scale'], model_file_path
+        return scale_dict['scaled_model_scale'], model_rescale, model_file_path
 
     def getAll(self):
         """Get all information in the database"""
@@ -163,12 +166,11 @@ def read_off(file):
     faces = [[int(s) for s in file.readline().strip().split(' ')][1:] for i_face in range(n_faces)]
     return verts, faces 
  
-def show_model_points(verts):
+def show_model_points(verts, col='r', mark='o',doit=False):
     print("Num points: ",len(verts))
     x_vals = list()
     y_vals = list()
     z_vals = list()
-    
     for p in verts:
         x_vals.append(p[0])
         y_vals.append(p[1])
@@ -176,11 +178,33 @@ def show_model_points(verts):
 
     from matplotlib import pyplot
     from mpl_toolkits.mplot3d import Axes3D
-    
     fig = pyplot.figure()
     ax = Axes3D(fig)
     
-    ax.scatter(x_vals, y_vals, z_vals, c='r', marker='o')
+    ax.scatter(x_vals, y_vals, z_vals, c=col, marker=mark)
+    pyplot.show()
+
+def getXYZ(verts,scale=1):
+    print("Num points: ",len(verts))
+    x_vals = list()
+    y_vals = list()
+    z_vals = list()
+    
+    for p in verts:                      
+        x_vals.append(p[0]*scale)
+        y_vals.append(p[1]*scale)                  
+        z_vals.append(p[2]*scale)
+    return x_vals, y_vals, z_vals
+
+
+def show_two_plots(v1, v2):
+    from matplotlib import pyplot
+    from mpl_toolkits.mplot3d import Axes3D
+    fig = pyplot.figure()
+    ax = Axes3D(fig)
+    
+    ax.scatter(v1[0], v1[1], v1[2], c='r', marker='o')
+    ax.scatter(v2[0], v2[1], v2[2], c='b', marker='x')
     pyplot.show()
 
 py_dir = os.path.dirname(os.path.abspath(__file__))
@@ -215,16 +239,24 @@ if __name__ == '__main__':
     one_grasp = batch[0]
     params = config(section='data')
 
-    scale, model_path = mr.getModelInfo(one_grasp["scaled_model_id"])
+    scale,rescale, model_path = mr.getModelInfo(one_grasp["scaled_model_id"])
     model_file = open(params['model_dir'] + model_path)
-    from pyntcloud import PyntCloud
-    m = PyntCloud.from_file(params['model_dir'] + model_path)
-    print(m)
-    m.plot(mesh=True, backend="matplotlib")
+    #from pyntcloud import PyntCloud
+    #m = PyntCloud.from_file(params['model_dir'] + model_path)
+    #print(m)
+    #m = m.get_sample("points_random", n = 5000)
+    #m.plot(mesh=True, backend="matplotlib")
+    print(len(one_grasp['grasp_contacts']))
+    contacts = one_grasp['grasp_contacts']
 
-
-    #verts, faces = read_off(model_file)
+    verts, faces = read_off(model_file)
     #show_model_points(verts)
+
+    print("scale: " + str(scale))
+    print("rescale: " + str(rescale))
+
+    show_two_plots(getXYZ(verts,scale*rescale), getXYZ([contacts[x:x+3] for x in range(0,len(contacts),3)]))
+
    
     #
     # Write joints to file
